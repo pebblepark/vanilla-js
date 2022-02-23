464478762b625ef653c78659edab7b6d34358968
pebblepark <subling02@gmail.com>
2022-02-24 00:37:00 +0900

    refactor: Items, ItemAppender, ItemFilter 컴포넌트 분할
    - entry point를 main.js로 변경
    - Items에 존재하던 로직을  App.js로 넘겨줌
    - Items, ItemAppender, ItemFilter는 App.js에서 넘겨주는 props를 활용해 로직 처리

--- src/app.js
+++ src/app.js
@@ -1,10 +1,80 @@
+import Component from './core/Component.js';
+import ItemAppender from './components/ItemAppeder.js';
+import ItemFilter from './components/ItemFilter.js';
 import Items from './components/Items.js';
 
-class App {
-  constructor() {
-    const $app = document.querySelector('#app');
-    new Items($app);
+export default class App extends Component {
+  setup() {
+    this.$state = {
+      isFilter: 0,
+      items: [
+        { seq: 1, contents: 'item1', active: false },
+        { seq: 2, contents: 'item2', active: true },
+      ],
+    };
+  }
+
+  template() {
+    return `
+      <header data-component="item-appender"></header>
+      <main data-component="items"></main>
+      <footer data-component="item-filter"></footer>
+    `;
+  }
+
+  mounted() {
+    const { filteredItems, addItem, deleteItem, toggleItem, filterItem } = this;
+    const $itemAppender = document.querySelector(
+      '[data-component="item-appender"]'
+    );
+    const $items = document.querySelector('[data-component="items"]');
+    const $itemFilter = document.querySelector(
+      '[data-component="item-filter"]'
+    );
+
+    new ItemAppender($itemAppender, { addItem: addItem.bind(this) });
+    new Items($items, {
+      filteredItems,
+      toggleItem: toggleItem.bind(this),
+      deleteItem: deleteItem.bind(this),
+    });
+    new ItemFilter($itemFilter, { filterItem: filterItem.bind(this) });
+  }
+
+  get filteredItems() {
+    const { isFilter, items } = this.$state;
+    return items.filter(
+      ({ active }) =>
+        (isFilter === 1 && active) ||
+        (isFilter === 2 && !active) ||
+        isFilter === 0
+    );
+  }
+
+  addItem(contents) {
+    const { items } = this.$state;
+    const seq = Math.max(0, ...items.map((item) => item.seq)) + 1;
+    const active = false;
+    this.setState({ items: [...items, { seq, contents, active }] });
+  }
+
+  deleteItem(seq) {
+    const items = [...this.$state.items];
+    items.splice(
+      items.findIndex((v) => v.seq === seq),
+      1
+    );
+    this.setState({ items });
+  }
+
+  toggleItem(seq) {
+    const items = [...this.$state.items];
+    const index = items.findIndex((v) => v.seq === seq);
+    items[index].active = !items[index].active;
+    this.setState({ items });
+  }
+
+  filterItem(isFilter) {
+    this.setState({ isFilter });
   }
 }
-
-new App();
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,34 +1,11 @@
 import Component from '../core/Component.js';
 
 export default class Items extends Component {
-  get filteredItems() {
-    const { isFilter, items } = this.$state;
-    return items.filter(
-      (item) =>
-        (isFilter === 1 && item.active) ||
-        (isFilter === 2 && !item.active) ||
-        isFilter === 0
-    );
-  }
-
-  setup() {
-    this.$state = {
-      isFilter: 0,
-      items: [
-        { seq: 1, contents: 'item1', active: false },
-        { seq: 2, contents: 'item2', active: true },
-      ],
-    };
-  }
-
   template() {
-    const { items } = this.$state;
+    const { filteredItems } = this.$props;
     return `
-      <header>
-        <input type="text" class="appender" placeholder="아이템 내용 입력" />
-      </header>
       <ul>
-          ${this.filteredItems
+          ${filteredItems
             .map(
               ({ seq, contents, active }) =>
                 `<li data-seq="${seq}">
@@ -43,42 +20,18 @@ export default class Items extends Component {
             )
             .join('')}
       </ul>
-      <button class="filterBtn" data-is-filter="0">전체 보기</button> 
-      <button class="filterBtn" data-is-filter="1">활성 보기</button> 
-      <button class="filterBtn" data-is-filter="2">비활성 보기</button> 
     `;
   }
 
   setEvent() {
-    this.addEvent('keyup', '.appender', ({ key, target }) => {
-      if (key !== 'Enter') return;
-      const { items } = this.$state;
-      const seq = Math.max(0, ...items.map((item) => item.seq)) + 1;
-      const contents = target.value;
-      const active = false;
-      this.setState({ items: [...items, { seq, contents, active }] });
-    });
+    const { deleteItem, toggleItem } = this.$props;
 
     this.addEvent('click', '.deleteBtn', ({ target }) => {
-      const items = [...this.$state.items];
-      const seq = Number(target.closest('[data-seq]').dataset.seq);
-      items.splice(
-        items.findIndex((v) => v.seq === seq),
-        1
-      );
-      this.setState({ items });
+      deleteItem(Number(target.closest('[data-seq]').dataset.seq));
     });
 
     this.addEvent('click', '.toggleBtn', ({ target }) => {
-      const items = [...this.$state.items];
-      const seq = Number(target.closest('[data-seq]').dataset.seq);
-      const index = items.findIndex((v) => v.seq === seq);
-      items[index].active = !items[index].active;
-      this.setState({ items });
-    });
-
-    this.addEvent('click', '.filterBtn', ({ target }) => {
-      this.setState({ isFilter: Number(target.dataset.isFilter) });
+      toggleItem(Number(target.closest('[data-seq]').dataset.seq));
     });
   }
 }
